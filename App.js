6e4aeb8ee641fe645ba93840c0d12559334baf4b
TurboMca <mcaucc04@gmail.com>
2025-09-18 17:57:35 +0530

    Update App.js

--- App.js
+++ App.js
@@ -55,7 +55,7 @@ export default function App() {
           {!hasError && (
             <WebView
               key={reloadKey}
-              source={{ uri: 'https://app.modconelectromech.com/Identity/Account/Login?companyCode=MOD' }}
+              source={{ uri: 'https://enelistrainfotech.pythonanywhere.com' }}
               style={{ flex: 1 }}
               onLoadEnd={handleLoadEnd}
               onError={handleError}
